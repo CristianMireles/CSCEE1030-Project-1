ef9ba100697cef010cec08fa701d762bf6008afb
SamuelSmethers <ssmethers16@yahoo.com>
2026-03-20 02:21:23 +0000

    adding more of my sections and integrating our variable names

--- Project1Test.cpp
+++ Project1Test.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main()
 {
     string userName;
-    bool userNameAccepted = false;
+    bool userNameAccepted = false; //variable to update wether the username is good or not
 	cout<<"Enter your username using letters, numbers, and spaces: "<<endl;
 	getline(cin, userName);
 	
@@ -28,7 +28,6 @@ int main()
 	}
 	cout<<"Hello "<< userName<< "!"<< endl;
 
-    int playerPoints = 50;
     int randNum1, randNum2;
     enum Menu {Multiply = 1, Divide = 2, Average = 3, Reveal = 4, GiveUp = 5, Exit = 6};
 
@@ -41,8 +40,15 @@ int main()
     } while (randNum1 > randNum2);
     //cout << randNum1 << " " << randNum2 << endl; This was to check output of numbers
 
-    bool stopPlaying = false;
-    int menuChoice;
+    bool stopPlaying = false; // if they chose to stop playing update this to true
+    bool revealNum = false; // if user choses to reveal number update this variable to have the value revealed
+    bool correctAnswer == false; // update if they get correct so that we can ask further question about restarting
+    char restartGame; // if user wins ask if they want to restart the game and update this variable
+
+    int playerPoints = 50; //intialize thier points update throughout game
+    int menuChoice; //collect users choice for game
+    float playerGuess = 0.00;   // float to get decimals
+
 
     do
     {
@@ -53,16 +59,35 @@ int main()
 		cout<<"4. Reveal"<< endl;
 		cout<<"5. Giveup"<< endl;
 		cout<<"6. Exit"<< endl;
+        if(revealNum == true)
+        {
+            cout<< "The first random number is " << randNum1;
+        }
 		cout<<"Enter choice: ";
 		cin>>menuChoice;
 
         switch(menuChoice)
         {
+            case Multiply:
+				cout<< "Enter your guess for the product: ";
+				cin>>playerGuess;
+				if (((randNum1 * randNum2)-abs(playerGuess)<50) && ((randNum1 * randNum2)-abs(playerGuess)>-50))
+				{
+					cout<<"Your guess was close enough. You win."<< endl;
+					playerPoints+=5;
+					cout<< "Your updated points are: " << playerPoints<< endl;
+					correctAnswer=true;
+				}
+				else
+				{
+					cout<< "Wrong guess. " << endl;
+					playerPoints-=1;
+				}
+                break;
             case Divide:
                 cout << "Guess the qoutient of the two randum numbers only input 2 numbers after decimal(Example 0.45)" << endl;
-                float guess;// float to get decimals
-                cin >> guess;
-                if(guess >= (randNum1/randNum2)-(0.5) && guess <= (randNum1/randNum2)+(0.5))
+                cin >> playerGuess;
+                if(guess >= (randNum1/randNum2)-(0.5) && playerGuess <= (randNum1/randNum2)+(0.5))
                 {
                     playerPoints = playerPoints + 5;
                     cout << "Your guess was correct and gain 5 points." << endl;
@@ -77,6 +102,9 @@ int main()
                 }
                 cout << randNum1 / randNum2 << endl;
                 break;
+            case Average:
+                
+                break;
             case Reveal:
                 cout << "Using 3 points to reveal 1st number." << endl; 
                 cout << "The first number is " << randNum1 << endl;
@@ -86,8 +114,12 @@ int main()
             case Exit:
                 cout << "Thanks for playing " << userName << " final score: " << playerPoints << endl;
                 cout << "Exiting the program" << endl;
+                stopPlaying = true;
                 break;
+            default:
+                cout << "Wrong choice. Please enter again."<< endl;
         }
+        cout<< endl;
     }while(stopPlaying == false);
 
     return 0;
